811a96b9a167f0a88beaf1ab4d2e1439dcf604fa
Burak Tasel <buraktasel@Buraks-MBP.fios-router.home>
2019-12-08 15:37:20 -0500

    Update stage

--- src/main/java/com/btasel/cicd/calc/CalcApplication.java
+++ src/main/java/com/btasel/cicd/calc/CalcApplication.java
@@ -3,6 +3,9 @@ package com.btasel.cicd.calc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Application.
+ */
 @SpringBootApplication
 public class CalcApplication {
 
--- src/main/java/com/btasel/cicd/calc/Calculator.java
+++ src/main/java/com/btasel/cicd/calc/Calculator.java
@@ -2,6 +2,9 @@ package com.btasel.cicd.calc;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator class.
+ */
 @Service
 public class Calculator {
      int sum(int a, int b) {
